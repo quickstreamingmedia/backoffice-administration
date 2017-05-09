bde21f47f6bdc7d90e33024d9fc5ce4822d9c0db
Venkatesh Gajula <quickstreamingmedia@users.noreply.github.com>
2017-05-09 10:27:30 +0700

    added db

--- config/env/db.connection.js
+++ config/env/db.connection.js
@@ -1,11 +1,11 @@
 
 module.exports = {
-  name: process.env.DB_NAME || "DB_NAME",
-  host: process.env.DB_HOST || "DB_HOST",
+  name: process.env.DB_NAME || "heroku_f614ee89348daac",
+  host: process.env.DB_HOST || "us-cdbr-east.cleardb.com/heroku_db?reconnect=true",
   port: process.env.DB_PORT || 3306, // replace with your database port, 3306 is for mysql, 5432 for postgres
-  username: process.env.DB_USERNAME || "DB_USERNAME",
-  password: process.env.DB_PASSWORD || "DB_PASSWORD",
-  dialect: process.env.DB_DIALECT || 'DB_DIALECT', //mysql, postgres, sqlite3,...
+  username: process.env.DB_USERNAME || "bd4713586d4b7a",
+  password: process.env.DB_PASSWORD || "ec8d0508",
+  dialect: process.env.DB_DIALECT || 'mysql', //mysql, postgres, sqlite3,...
   storage: "./db.development.sqlite",
   enableSequelizeLog: process.env.DB_LOG || true,
   ssl: process.env.DB_SSL || false,
